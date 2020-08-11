97cb5ab1957617a8ebad4f922d77add79c070aad
slowik <aleksandra.slowik88.gmail.com>
2020-08-11 20:30:06 +0200

    erreur images trouvé

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function mots_a_trouver()
 {
-	return 	str = "Trigonometrique : Un cercle*Denominateur : Dans une fraction, le numerateur et le...*Abscisse : L'une des coordonnees d'un point du plan*Ordonnee : L'une des coordonnees d'un point du plan*Paralleles : Deux droites de meme direction*Perpendiculaires : Deux droites a 90 degres*Le perimetre : 2xPIxR est -- --------- d'un cercle*aire : PIxR<sup>2</sup> est L'---- d'un cercle*Segment : [AB]xPythagore : La somme des carres des cotes est egale au carre de l'Hypotenuse*Triangle : La somme de ses angles est 180 degres*Concentriques : Des cercles de memes centres*inverse : 1/2 est L'------- de 2*oppose : -2 est L'------ de 2*Factoriser : Reduire une equation*Rectangle : Un triangle ABC avec un angle de 90 degres en A*Translation : Dans le plan, transformer le point A en B*Demontrer : CQFD, Ce qu'il fallait...*Discriminant : Sert a trouver les solutions d'une equation du 2nd degre*Identite remarquable : (x - 1)<sup>2</sup> = x<sup>2</sup> - 2x + 1*Nombre premier : N'est divisible que par 1 et lui meme*Nombre entier : N'a pas de decimale*Mediatrice : Coupe orthogonalement un segment en son milieu*Bissectrice : Divise un angle en deux parties egales*Thales : Des rapports egaux dans un triangle, theoreme de*Vecteur : Le ------- directeur d'une droite*Derivee : 2x est la ------- de x<sup>2</sup>*integrale : x<sup>2</sup>/2 est L'--------- de x*Tangente : Le sinus d'un angle divise par le cosinus du meme angle*Logarithme : Le ---------- neperien*Valeur absolue : |x|*Inequation : -5x<-2*Racine carre : 4 est la ------ ----- de 16*Trinome : Equation du 2nd degre*Symetrique : Le point (2,2) est le point ---------- de (-2,-2) par rapport au point (0,0)*Limite : Lorsque X tend vers l'infini, 1/X tend vers 0, on parle de*Parabole : La representation graphique de la fonction f(x) = x<sup>2</sup> est une*Complexe : i<sup>2</sup> = -1, Nombre*Logarithme Neperien : Ln*Logarithme Decimal : Log*Exponentielle : e<sup>2</sup>*Derivee : uv = u'v + uv'*Multiplication : Est prioritaire sur l'addition*Division : Est prioritaire sur l'addition";
+	return 	str = "La phrase Quand je suis content je vomis viens du film*La cité de la peur : La vérité si je*Mens : L'une des coordonnees d'un point du plan*Ordonnee : L'une des coordonnees d'un point du plan*Paralleles : Deux droites de meme direction*Perpendiculaires : Deux droites a 90 degres*Le perimetre : 2xPIxR est -- --------- d'un cercle*aire : PIxR<sup>2</sup> est L'---- d'un cercle*Segment : [AB]xPythagore : La somme des carres des cotes est egale au carre de l'Hypotenuse*Triangle : La somme de ses angles est 180 degres*Concentriques : Des cercles de memes centres*inverse : 1/2 est L'------- de 2*oppose : -2 est L'------ de 2*Factoriser : Reduire une equation*Rectangle : Un triangle ABC avec un angle de 90 degres en A*Translation : Dans le plan, transformer le point A en B*Demontrer : CQFD, Ce qu'il fallait...*Discriminant : Sert a trouver les solutions d'une equation du 2nd degre*Identite remarquable : (x - 1)<sup>2</sup> = x<sup>2</sup> - 2x + 1*Nombre premier : N'est divisible que par 1 et lui meme*Nombre entier : N'a pas de decimale*Mediatrice : Coupe orthogonalement un segment en son milieu*Bissectrice : Divise un angle en deux parties egales*Thales : Des rapports egaux dans un triangle, theoreme de*Vecteur : Le ------- directeur d'une droite*Derivee : 2x est la ------- de x<sup>2</sup>*integrale : x<sup>2</sup>/2 est L'--------- de x*Tangente : Le sinus d'un angle divise par le cosinus du meme angle*Logarithme : Le ---------- neperien*Valeur absolue : |x|*Inequation : -5x<-2*Racine carre : 4 est la ------ ----- de 16*Trinome : Equation du 2nd degre*Symetrique : Le point (2,2) est le point ---------- de (-2,-2) par rapport au point (0,0)*Limite : Lorsque X tend vers l'infini, 1/X tend vers 0, on parle de*Parabole : La representation graphique de la fonction f(x) = x<sup>2</sup> est une*Complexe : i<sup>2</sup> = -1, Nombre*Logarithme Neperien : Ln*Logarithme Decimal : Log*Exponentielle : e<sup>2</sup>*Derivee : uv = u'v + uv'*Multiplication : Est prioritaire sur l'addition*Division : Est prioritaire sur l'addition";
 }
 let fin = true;
 let nb_erreurs = 0;
@@ -51,7 +51,7 @@ let nb_alea = Math.floor(Math.random() * nb_mots);
 
 le_score = le_score=nb_erreurs/4;
 document.getElementById('leScore').innerHTML = 'Votre score :<strong>' + le_score + ' / 10</strong> - Mots restants :<strong>' + (10 - nb_passe) + '</strong>'
-document.getElementById('lePendu').src = 'assets/img/images/img_pendu/pendu_defaut.png';
+document.getElementById('lePendu').src = 'assets/img/images/pendu_defaut.png';
 nb_erreurs=0;
 
 init_calques();
@@ -133,7 +133,7 @@ window.setTimeout(function attendre(){ suivant(); }, 1000);
 else
 {
 nb_erreurs++;
-document.getElementById('lePendu').src = 'assets/img/images/img_pendu/pendu' + nb_erreurs + '.png';
+document.getElementById('lePendu').src = 'assets/img/images/pendu' + nb_erreurs + '.png';
 if(nb_erreurs==4)
 {
 nb_passe++;
