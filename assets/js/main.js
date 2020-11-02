6997eafd7d00f9fee5a07b15bde5d183c0b10b06
aleks <aleksandra.slowik88@gmail.com>
2020-11-02 15:29:07 +0100

    bug rectif

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 function mots_a_trouver() {
 	return str = "*La cité de la peur : La phrase Quand je suis content je vomis viens du film *lion :Le roi des animaux : *Prince : Qui a chanté Purple Rain : *Râ : Le dieu du soleil dans L'ancien Egypte : *Victor Hugo : Miserables : *Federer : Joueur de tennis : *Nirvana : Smells like a ten spirit : *Watson : Qui raconte les aventures de Sherlock Holmes : *Clyde : L'inséparable de Bonnie : *always : ...Coca-cola : *Neil Amstrong : Premier pas sur la lune : *Jaques : Pierre Paul ... :*Esmeralda : A chaque Quasimodo sa... :* Jacouille la Fripouille : Jour! Nuit! Jour! Nuit! :*The Doors : Jim Morrison :*Vadrouille : La grande... : *Mr Bean : Rowan Atkinson :* Khaled : Aicha : *Peter Pan : Forever young : *Tommy Shelby : Peaky Blinders : *Titanic : My heart will go on : * Jamel Debouze : Numérobis : *Super Bowl : Foot Americain : *Claude : Mc Solar : *Bazinga : Sheldon : *Woody : Toy Story : *Kyo : Benoît Poher : *Police : Sting : *Mercury : Freddy : *Folles : La cage aux... : *Watch : The night... : * Lucille : Negan : * Stephen King : Ca : * Hannibal : Lecter : * Harley Quinn : Jocker : *Batman : Robin : *Foule Sentimentale : Souchon : *Les trois frères : Les inconus : *Taxi : Alors les filles on se laisse pousser la moustache? : *Dikkenek : Ou tu sors ou je te sors... : *Le dinner de con : Il s'appelle Juste Leblanc : *Dr Mamour : Greys Anatomy : *Prison Break : Geule d'ange : *Naruto : Razengan : * Hunter x Hunter :  Gon Freecss : *Hunger Games : Peeta : *Tomb Rider :Evangelina : *Da Vinci Code : Dan Brown : *Star Wars : Anakin : *Peaky Blinders : By order of the... : *Dexter : Morgan ";
-
 }
-
 let fin = true;
 let nb_erreurs = 0;
 let nb_passe = 0;
@@ -20,6 +18,9 @@ recuperer();
 function recuperer() {
 	tab_mots = mots_a_trouver().split('*');
 	nb_mots = tab_mots.length;
+	//alert(nb_mots);
+	//alert(tab_mots[0]);
+
 
 }
 
@@ -34,10 +35,14 @@ function debuter() {
 
 }
 
+function init_calques() {
+
+}
 
 function suivant() {
 	let tab_enigme;
 	let nb_alea = Math.floor(Math.random() * nb_mots);
+	//alert(nb_alea);
 
 	le_score = le_score = nb_erreurs / 4;
 	document.getElementById('leScore').innerHTML = 'Votre score :<strong>' + le_score + ' / 5</strong> - Mots restants :<strong>' + (5 - nb_passe) + '</strong>'
@@ -45,20 +50,16 @@ function suivant() {
 	nb_erreurs = 0;
 
 	init_calques();
-
 	let parent = document.getElementById('apercu');
 	let enfants = parent.getElementsByTagName('div');
-
 	for (var i = 0; i < enfants.length; i++) {
 		if (enfants[i].id != 'saut1' && enfants[i].id != 'saut2')
 			enfants[i].style.backgroundColor = '#EDEEEE';
-
 	}
 
 
 	while (chaine_rangee.indexOf('-' + nb_alea + '-') > -1) {
 		nb_alea = Math.floor(Math.random() * nb_mots);
-
 	}
 
 	chaine_rangee += '-' + nb_alea + '-';
@@ -82,6 +83,7 @@ function clavier(evenement) {
 
 	let touche = window.event ? evenement.keyCode : evenement.which;
 	touche = String.fromCharCode(touche).substr(0, 1);
+	//alert(touche);
 
 	if (touche == " ") {
 		la_touche = " ";
@@ -92,6 +94,7 @@ function clavier(evenement) {
 
 	document.getElementById("calque_" + touche.toLowerCase()).style.backgroundColor = "#2c1c1c";
 
+	//traitement récursif
 
 	for (indice = 0; indice <= mem_mot.length - 1; indice++) {
 		la_lettre = mem_mot.substr(indice, 1);
@@ -99,27 +102,21 @@ function clavier(evenement) {
 			trouve = true;
 			le_mot = le_mot.substr(0, indice) + la_lettre + le_mot.substr(indice + 1);
 			document.getElementById('leMot').innerHTML = le_mot;
-
 		}
 	}
-
 	if (trouve == true) {
 		if (le_mot == mem_mot) {
 			nb_passe++;
 			if (nb_passe == 5) {
 				document.getElementById('leScore').innerHTML = 'Votre score :<strong>' + (le_score - nb_erreurs / 4) + ' / 10</strong> - Mots restants : <strong>' + (5 - nb_passe) + '</strong>- <strong>Tu as gagné! Je te laiss partir cette fois.. </strong>';
 				fin = true;
-
 			}
-
 			else {
 				window.setTimeout(function attendre() { suivant(); }, 1000);
 				//alert('Perdu! Essaye encore si tu veut retrouver ton chemin...');
-
 			}
 		}
 	}
-
 	else {
 		nb_erreurs++;
 		document.getElementById('lePendu').src = 'assets/img/images/pendu' + nb_erreurs + '.png';
@@ -128,7 +125,6 @@ function clavier(evenement) {
 			if (nb_passe == 5)
 				fin = true
 			window.setTimeout(function attendre() { suivant(); }, 1000);
-
 		}
 	}
 }
